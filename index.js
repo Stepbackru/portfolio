3329e18572171df46999e7cdb5ca0314e5a0f476
Stepbackru <alex-ruba@bk.ru>
2022-01-30 15:59:52 +0300

    feat: added translatePage to main js file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import burgerMenu from './components/burgerMenu.js';
 import seasonsChange from './components/seasonsChange.js';
+import translatePage from './components/translatePage.js';
 
 window.onload = () => {
   burgerMenu.subscribe();
   seasonsChange.subscribe();
+  translatePage.subscribe();
 }
