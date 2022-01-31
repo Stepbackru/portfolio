5522c52980379f3d44c90145556794dffd975b47
Stepbackru <alex-ruba@bk.ru>
2022-01-31 22:56:15 +0300

    fix: refactor logic for fix all light theme classes

--- components/changeTheme.js
+++ components/changeTheme.js
@@ -3,16 +3,27 @@ import elemsForChangeThemeArr from '../data/theme.js';
 class ChangeTheme {
   constructor() {
     this.themeButton = document.querySelector('.theme__item');
+    this.logo = document.querySelector('.logo__icon');
 
     this.themeToggle = this.themeToggle.bind(this);
   }
 
   themeToggle(e) {
-    elemsForChangeThemeArr.forEach((el) => {
-      const item = document.querySelector(`.${el}`);
+    const items = [...document.querySelectorAll(`.${elemsForChangeThemeArr.join(',.')}`)];
+    items.forEach((el) => {
+      const classToChange = elemsForChangeThemeArr
+        .filter((classes) => classes === [...el.classList]
+          .find(elem => classes === elem));
 
-      item.classList.toggle(`${el}-lightTheme`);
+      console.log(classToChange)
+      classToChange.forEach(elem => el.classList.toggle(`${elem}-lightTheme`));
     });
+
+    if (e.target.classList.contains('theme__item-lightTheme')) {
+      this.logo.src = './assets/images/lightTheme/icons/logo.svg';
+    } else {
+      this.logo.src = './assets/icons/logo.svg';
+    }
   }
 
   subscribe() {
